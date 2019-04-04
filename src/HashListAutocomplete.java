363cbb80318dff82e4b175341e86f57984bf91b0
Tyler Meier <tkm22@duke.edu>
2019-04-04 00:56:42 -0400

    all done with everything

--- src/HashListAutocomplete.java
+++ src/HashListAutocomplete.java
@@ -6,6 +6,14 @@ public class HashListAutocomplete implements Autocompletor {
 	private Map<String, List<Term>> myMap;
 	private int mySize;
 	
+	/**
+	 * The constructor for the HashListAutocomplete class. 
+	 * @param terms, list of all the possible terms
+	 * @param weights, list of all weights corresponding to each term in terms
+	 * @throws NullPointerException, if terms or weights is null
+	 * @throws IllegalArgumentException, if the lengths of terms and weights don't match
+	 * 
+	 */
 	public HashListAutocomplete(String[] terms, double[] weights) {
 		if (terms == null || weights == null) {
 			throw new NullPointerException("One or more arguments null");
@@ -16,6 +24,14 @@ public class HashListAutocomplete implements Autocompletor {
 		initialize(terms, weights);
 	}
 
+	/**
+	 * Returns the top k matching terms in descending order of weight. If there
+	 * are fewer than k matches, return all matching terms in descending order
+	 * of weight. If there are no matches, return an empty list.
+	 * @param prefix, is the prefix being searched for in map
+	 * @param k, is the amount of top terms desired
+	 * @return top k matching terms or empty list depending on above
+	 */
 	@Override
 	public List<Term> topMatches(String prefix, int k) {
 		List<Term> all = myMap.get(prefix);
@@ -25,6 +41,14 @@ public class HashListAutocomplete implements Autocompletor {
 		return all.subList(0, Math.min(k, all.size()));
 	}
 
+	/**
+	 * Create internal state needed to store Term objects
+	 * from the parameters. Should be called in implementing
+	 * constructors. Creates a map with all prefixes and a list of terms
+	 * corresponding to each prefix
+	 * @param terms is array of Strings for words in each Term
+	 * @param weights is corresponding weight for word in terms
+	 */
 	@Override
 	public void initialize(String[] terms, double[] weights) {
 		
@@ -49,6 +73,13 @@ public class HashListAutocomplete implements Autocompletor {
 		}
 	}
 
+	/**
+	 * Return size in bytes of all Strings and doubles
+	 * stored in implementing class. To the extent that
+	 * other types are used for efficiency, there size should
+	 * be included too 
+	 * @return number of bytes used after initialization
+	 */
 	@Override
 	public int sizeInBytes() {	
 		if (mySize == 0) {
